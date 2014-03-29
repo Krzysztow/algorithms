fdc177e4fdaab8dbd68802b0988bf81783f7d084
Krzysztow <Krzysztow@gmail.com>
2014-03-29 10:56:27 +0000

    Third type of Stack added

--- cci/ex-3.1/main.cpp
+++ cci/ex-3.1/main.cpp
@@ -64,6 +64,11 @@ private:
 template <class T>
 T SimpleStack<T>::_defaultItem;
 
+/**
+ * Linked list implementation that divides entire array to the StackNode elements.
+ * Firstly they belong to the free list nodes. The pointer to such linked list is pointed
+ * to all LinkedStack constructors.
+ */
 template <class T>
 class ListStack:
     public Stack<T>
@@ -155,8 +160,120 @@ class ListStack:
 template <class T>
 T ListStack<T>::_defaultData;
 
+template <class T, int C = 5>
+class BlockStack:
+    public Stack<T> {
+    public:
+        struct StackPage {
+            T data[C];
+            StackPage *next;
+        };
+
+    static StackPage *initBuffer(char data[], int size) {
+        int no = size / sizeof(StackPage);
+        StackPage *head = (StackPage*)data;
+        StackPage *page = head;
+        for (int i = 0; i < no - 1; ++i) {
+            page->next = (page + 1);
+            page = page->next;
+        }
+
+        page->next = 0;
+
+        return head;
+    }
+
+    public:
+        BlockStack(StackPage **freeList):
+            _freeListPtr(freeList),
+            _size(0),
+            _head(0) {}
+
+        virtual bool push(const T &node) {
+            StackPage *p = _getPageWNextNode(_size);
+            if (0 == p) {
+                return false;
+            }
+            const int pIdx = _inPageIdx(_size);
+            p->data[pIdx] = node;
+            //alternatively memcpy(&(p->data[pIdx]), &node, sizeof(T));
+            if (p != _head) {//this could be moved to _getPageWNextNode()
+                p->next = _head;
+                _head = p;
+            }
+            ++_size;
+            return true;
+        }
+
+        virtual const T pop() {
+            if (0 == _size)
+                return _defaultData;
+
+            const int pIdx = _inPageIdx(_size - 1);
+            T value = _head->data[pIdx];
+            if (0 == pIdx) {
+                StackPage *next = _head->next;
+                _returnPage(_head);
+                _head = next;
+            }
+            --_size;
+            return value;
+        }
+
+        virtual int size() {
+            return _size;
+        }
+
+    private:
+        StackPage *_getPageWNextNode(int nodeIdx) {
+            int inPageIdx = _inPageIdx(nodeIdx);
+            StackPage *p = 0;
+            if (0 == inPageIdx) {//we require next page
+                if (0 == *_freeListPtr) {
+                    return 0;
+                }
+                p = *_freeListPtr;
+                *_freeListPtr = (*_freeListPtr)->next;
+            }
+            else {
+                p = _head;
+            }
+
+            return p;
+        }
+
+        void _returnPage(StackPage *p) {
+            p->next = *_freeListPtr;
+            *_freeListPtr = p;
+        }
+
+        int _pageIndex(int globalIdx) {
+            return globalIdx / C;
+        }
+
+        int _inPageIdx(int globalIdx) {
+            return globalIdx % C;
+        }
+
+    private:
+        StackPage **_freeListPtr;
+        int _size;
+
+        StackPage *_head;
+        static T _defaultData;
+};
+
+template<class T, int C>
+T BlockStack<T, C>::_defaultData;
+
+void pushNewData(Stack<int> *stack, int data) {
+    if (! stack->push(data)) {
+        fprintf(stderr, "Cannot add data %d\n", data);
+    }
+}
+
 int main(int argc, const char * argv[]) {
-    const int BuffSize = 8;
+    const int BuffSize = 16;
 
     int buffer[BuffSize];
     SimpleStack<int> stack(buffer, BuffSize);
@@ -167,17 +284,25 @@ int main(int argc, const char * argv[]) {
     b2 = ListStack<int>::initBuffer(buffer2, BuffByteSize);
     ListStack<int> stack2(&b2);
 
+    char buffer3[BuffByteSize];
+    BlockStack<int, 2>::StackPage *b3 = BlockStack<int, 2>::initBuffer(buffer3, BuffByteSize);
+    BlockStack<int, 2> stack3(&b3);
+
     std::list<Stack<int>*> stacks;
     stacks.push_back(&stack);
     stacks.push_back(&stack2);
+    stacks.push_back(&stack3);
 
     std::list<Stack<int>*>::iterator it = stacks.begin();
 
     for (; stacks.end() != it; ++it) {
+        printf("-------------------------------------\n");
+
         Stack<int> *s = *it;
 
-        s->push(1);
-        s->push(2);
+        for (int i = 0; i < 10; ++i) {
+            pushNewData(s, i);
+        }
 
         while (0 != s->size()){
             printf("Pop: %d\n", s->pop());
